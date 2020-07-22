749d89b960b46917dfa38153c6828357288b6f88
Daniel Walsh <walshydev@gmail.com>
2020-07-22 15:30:06 +0100

    picky :p

--- src/main/java/dev/walshy/sfmetrics/charts/MetricsAutoUpdatesChart.java
+++ src/main/java/dev/walshy/sfmetrics/charts/MetricsAutoUpdatesChart.java
@@ -3,9 +3,9 @@ package dev.walshy.sfmetrics.charts;
 import io.github.thebusybiscuit.slimefun4.implementation.SlimefunPlugin;
 import org.bstats.bukkit.Metrics;
 
-public class MetricAutoUpdatesChart extends Metrics.SimplePie {
+public class MetricsAutoUpdatesChart extends Metrics.SimplePie {
 
-    public MetricAutoUpdatesChart() {
+    public MetricsAutoUpdatesChart() {
         super("metrics_auto_updates", () -> {
             boolean enabled = SlimefunPlugin.getMetricsService().hasAutoUpdates();
             return enabled ? "enabled" : "disabled";
