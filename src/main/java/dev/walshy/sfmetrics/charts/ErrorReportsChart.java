d6cef14d06f33b160f1160e69c99834729767c62
TheBusyBiscuit <mrCookieSlime@gmail.com>
2020-09-18 12:17:53 +0200

    Added Error Reports Chart

---
+++ src/main/java/dev/walshy/sfmetrics/charts/ErrorReportsChart.java
@@ -0,0 +1,54 @@
+package dev.walshy.sfmetrics.charts;
+
+import org.bstats.bukkit.Metrics.SingleLineChart;
+
+import com.google.gson.JsonObject;
+
+import dev.walshy.sfmetrics.VersionDependentChart;
+import io.github.thebusybiscuit.slimefun4.api.ErrorReport;
+import io.github.thebusybiscuit.slimefun4.api.SlimefunBranch;
+
+/**
+ * This single line graph shows the amount of {@link ErrorReport ErrorReports} generated.
+ * 
+ * @author TheBusyBiscuit
+ *
+ */
+public class ErrorReportsChart extends SingleLineChart implements VersionDependentChart {
+
+    // This needs to be static due to it being passed to the super constructor
+    private static int lastValue = 0;
+
+    public ErrorReportsChart() {
+        super("error_reports", () -> {
+            int currentValue = ErrorReport.count();
+            int amount = currentValue - lastValue;
+            lastValue = currentValue;
+            return amount;
+        });
+    }
+
+    @Override
+    public boolean isCompatible(SlimefunBranch branch, int build) {
+        if (branch == SlimefunBranch.DEVELOPMENT) {
+            return build >= 638;
+        }
+        else if (branch == SlimefunBranch.STABLE) {
+            return build >= 16;
+        }
+        else {
+            return false;
+        }
+    }
+
+    @Override
+    public String getName() {
+        return "Error-Reports";
+    }
+
+    @Override
+    public JsonObject getDataSample() throws Exception {
+        return getChartData();
+    }
+
+}
